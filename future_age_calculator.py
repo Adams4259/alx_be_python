916be53170df9c72f7a0fed2e597211ff4449a7a
Adams4259 <adamsechwa18@gmail.com>
2025-09-11 19:38:23 +0300

    add user input age calculator

---
+++ future_age_calculator.py
@@ -0,0 +1,11 @@
+# user input age calculator
+
+# Variables
+current_age = 25  # Current age in years
+years_to_add = 5  # Number of years to add
+future_age = current_age + years_to_add
+
+# Output the future age
+print("In", years_to_add, "years, you will be", future_age, "years old.")
+
+# --- IGNORE ---S
