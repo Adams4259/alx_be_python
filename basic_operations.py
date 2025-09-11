958db5345cb0f3e8ea59937c1d4a276446b9e660
Adams4259 <adamsechwa18@gmail.com>
2025-09-11 19:18:47 +0300

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,17 @@
+#Basic Arithmetic Operations
+
+#variables
+number1 = 10
+number2 = 5
+
+#Addition
+addition = number1 + number2
+print("Addition of", number1, "and", number2, "is:", addition)
+
+#Subtraction
+subtraction = number1 - number2
+print("Subtraction of", number2, "from", number1, "is:", subtraction)
+
+#Multiplication
+multiplication = number1 * number2
+print("Multiplication of", number1, "and", number2, "is:", multiplication)
